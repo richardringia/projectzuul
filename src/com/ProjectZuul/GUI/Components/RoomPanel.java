98a572150e69d14969643452c260809833577ab3
Richard Ringia <r.ringia99@gmail.com>
2020-01-23 21:32:00 +0100

    Javadoc RoomPanel

--- src/com/ProjectZuul/GUI/Components/RoomPanel.java
+++ src/com/ProjectZuul/GUI/Components/RoomPanel.java
@@ -6,10 +6,23 @@ import com.ProjectZuul.Models.Room;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * The roompanel is used in the maphandler for showing the rooms
+ */
 public class RoomPanel extends JPanel {
 
+    /**
+     * The room for the room panel
+     */
     private Room room;
 
+    /**
+     * Instantiates a new Room panel.
+     * If there is no room then the room panel is no empty and has no text or border
+     *
+     * @param room      the room for the room panel
+     * @param dimension the dimension of the panel
+     */
     public RoomPanel(Room room, Dimension dimension) {
         super(new GridLayout());
         this.room = room;
@@ -23,16 +36,29 @@ public class RoomPanel extends JPanel {
         this.setBackground(Color.BLACK);
     }
 
+    /**
+     * Get the room of the room panel
+     *
+     * @return the room
+     */
     public Room getRoom() {
         return this.room;
     }
 
+    /**
+     * Set the room panel to active.
+     * This is used when a player enters the room
+     */
     public void setActive() {
         this.setBackground(Color.WHITE);
         this.getComponent(0).setForeground(Color.BLACK);
         this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
     }
 
+    /**
+     * Set the room panel not active
+     * This is used when a player leaves the room
+     */
     public void setNotActive() {
         this.setBackground(Color.BLACK);
         this.getComponent(0).setForeground(Color.WHITE);
