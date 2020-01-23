3e6365f7cbf3f885bff95efe2db953d4601861bb
Richard Ringia <r.ringia99@gmail.com>
2020-01-23 21:26:15 +0100

    Javadoc actionmenu

--- src/com/ProjectZuul/GUI/ActionMenu.java
+++ src/com/ProjectZuul/GUI/ActionMenu.java
@@ -1,6 +1,5 @@
 package com.ProjectZuul.GUI;
 
-import com.ProjectZuul.Enums.Language;
 import com.ProjectZuul.Handlers.LanguageHandler;
 import com.ProjectZuul.Models.Player;
 
@@ -9,14 +8,37 @@ import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
+
+/**
+ * The type Action menu.
+ */
 public class ActionMenu extends JPanel {
+
+    /**
+     * The height of the panel
+      */
     private int height = 200;
+
+    /**
+     * The width of the panel
+     */
     private int width = 150;
 
+    /**
+     * The inner action panel where the action will be stored
+     */
     private JPanel actionPanel;
 
+    /**
+     * Handles all the text in the application with the right language
+     */
     private LanguageHandler languageHandler;
 
+    /**
+     * Instantiates a new Action menu.
+     *
+     * @param player the player of the game
+     */
     public ActionMenu(Player player) {
         this.languageHandler = player.getLanguageHandler();
         this.setBounds(600, 20, 150, 200);
@@ -28,6 +50,10 @@ public class ActionMenu extends JPanel {
     }
 
 
+    /**
+     * Create an title label for the action menu
+     * Add the action menu to the super (JPanel)
+     */
     private void createTitleLabel() {
         JLabel jLabel = new JLabel(languageHandler.get("GAME_ACTION_MENU_TITLE"));
         jLabel.setBounds(0, 0, 150, 30);
@@ -38,6 +64,10 @@ public class ActionMenu extends JPanel {
         super.add(jLabel);
     }
 
+    /**
+     * Create the action panel where the buttons will be placed in.
+     * Add the action panel to the super (JPanel)
+     */
     private void createActionMenuPanel() {
         this.actionPanel = new JPanel();
         this.actionPanel.setBounds(0, 30, this.width, this.height - 30);
@@ -47,10 +77,19 @@ public class ActionMenu extends JPanel {
         super.add(this.actionPanel);
     }
 
+    /**
+     * When a new component needs to be added to this panel. It need to be placed in the action panel.
+     * Therefore a custom add function is created
+     * @param component the component that needs to be added
+     * @return the created component
+     */
     public Component add(Component component) {
         return this.actionPanel.add(component);
     }
 
+    /**
+     * Reset the action menu, so remove all the buttons and update the ui
+     */
     public void reset() {
         this.actionPanel.removeAll();
         this.actionPanel.updateUI();
