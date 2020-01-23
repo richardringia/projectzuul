7830a7f0769e777ae3c04adda954db9f49cdd496
Richard Ringia <r.ringia99@gmail.com>
2020-01-23 23:00:23 +0100

    Audio temp

--- src/com/ProjectZuul/GUI/GameUI.java
+++ src/com/ProjectZuul/GUI/GameUI.java
@@ -223,6 +223,7 @@ public class GameUI implements SetInactiveListener {
             if (clockTime >= duration - 30000) {
                 timeLeftText.setForeground(Color.RED);
                 timeLabel.setForeground(Color.RED);
+                soundHandler.playScreamingGoatSound();
             }
 
             SimpleDateFormat df = new SimpleDateFormat("mm:ss");
